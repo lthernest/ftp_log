e943161e8ecab531aa2536c03a352f7efff7a633
U-HKBUAD\ernestlam <ernestlam@3417EBD3E21A.hkbuad.local>
2018-07-27 18:56:16 +0800

    droplevels script (not complete)

--- ftp_log.R
+++ ftp_log.R
@@ -351,8 +351,12 @@ return(incomplete_list)
 top10_incomplete_china_ip_list <- data.frame((top10_incomplete_china_ip), ip_retrieve(top10_incomplete_china_ip$Var1, "org"), (ip_retrieve(top10_incomplete_china_ip$Var1, "region")))
 
 check_incomplete_ip <- function (ip_list){
-  ip_variable <- incomplete_log_data_china[which(incomplete_log_data_china$ip == ip_list),]
-  check_result <- data.frame(table(droplevels(ip_variable$mm)),table(droplevels(ip_variable$doi)))
+  
+  for(i in seq_along(ip_list)){
+  ip_variable[i] <- incomplete_log_data_china[which(incomplete_log_data_china$ip == ip_list),]
+  
+  check_result <- data.frame(table(droplevels(ip_variable[i]$mm)),table(droplevels(ip_variable[i]$doi)))
+  }
   return(check_result)
 
 }
